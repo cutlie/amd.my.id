3f96d9a98ae689bb4ccebb07deb061cd59bfa320
cutlie <77072120+cutlie@users.noreply.github.com>
2024-02-20 01:47:04 +0700

    Update headcode.js

--- headcode.js
+++ headcode.js
@@ -1,4 +1,5 @@
 ///// Histats /////
+   <!-- Histats.com  START  (aync)-->
 <script type="text/javascript">var _Hasync= _Hasync|| [];
 _Hasync.push(['Histats.start', '1,4845975,4,0,0,0,00010000']);
 _Hasync.push(['Histats.fasi', '1']);
@@ -8,8 +9,8 @@ var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async
 hs.src = ('//s10.histats.com/js15_as.js');
 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
 })();</script>
-<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4845975&101" alt="web statistics" border="0"></a></noscript>
-
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4845975&101" alt="free website stats program" border="0"></a></noscript>
+<!-- Histats.com  END  -->
 ///// GA Analytics /////
 
 ///// Statcounter /////
