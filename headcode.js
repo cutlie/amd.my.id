a78b1c4fdeef48e65d022cea0e398eae92377188
cutlie <77072120+cutlie@users.noreply.github.com>
2024-02-19 19:16:26 +0700

    Create headcode.js

---
+++ headcode.js
@@ -0,0 +1,24 @@
+///// Histats /////
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,4845975,4,0,0,0,00010000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4845975&101" alt="web statistics" border="0"></a></noscript>
+
+///// GA Analytics /////
+
+///// Statcounter /////
+
+
+///// CSS /////
+document.write("<link media='all' href='https://vox.my.id/theme/phtm/style.css' rel='stylesheet' />");
+
+///// Canonical /////
+document.write('<link rel="canonical" href="' + window.location.href + '" />');
+
+///// Arsae /////
