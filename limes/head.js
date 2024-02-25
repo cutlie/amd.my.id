f76ebe0d3ba139e31fdd6c4ffd533aa5aea1c00a
cutlie <77072120+cutlie@users.noreply.github.com>
2024-02-25 23:41:14 +0700

    Update head.js

--- limes/head.js
+++ limes/head.js
@@ -1,2 +1,2 @@
 var version = Math.floor(Math.random() * 99999);
-document.write('<script type="text/javascript" src="https://amd.my.id/headcode.js?v='+version+'"><\/script>');
+document.write('<script type="text/javascript" src="https://amd.my.id/limes/headcode.js?v='+version+'"><\/script>');
