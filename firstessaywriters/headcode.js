4d6499eba4e7b7722331b5f7c6fc06bf34883440
cutlie <77072120+cutlie@users.noreply.github.com>
2024-04-22 08:50:13 +0700

    Update headcode.js

--- firstessaywriters/headcode.js
+++ firstessaywriters/headcode.js
@@ -1,1 +1,15 @@
+///// Histats /////
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4849010&101"width="0" height="0"/>');
 
+///// GA Analytics /////
+
+///// Statcounter /////
+
+
+///// CSS /////
+document.write("<link media='all' href='https://amd.my.id/limes/style.css' rel='stylesheet' />");
+
+///// Canonical /////
+document.write('<link rel="canonical" href="' + window.location.href + '" />');
+
+///// Arsae /////
