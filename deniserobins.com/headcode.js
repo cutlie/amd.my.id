00ba409689c845ab4ff485c34731484bc0e8ad85
cutlie <77072120+cutlie@users.noreply.github.com>
2024-05-13 18:55:38 +0700

    Update headcode.js

--- deniserobins.com/headcode.js
+++ deniserobins.com/headcode.js
@@ -7,7 +7,7 @@ document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4847459&101"w
 
 
 ///// CSS /////
-document.write("<link media='all' href='https://amd.my.id/deniserobins.com/style.css' rel='stylesheet' />");
+document.write("<link media='all' href='https://amd.my.id/denise/style.css' rel='stylesheet' />");
 
 ///// Canonical /////
 document.write('<link rel="canonical" href="' + window.location.href + '" />');
