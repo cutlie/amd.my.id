13517a9ca5767e0f2d726c5cc2263eb929222ccb
cutlie <77072120+cutlie@users.noreply.github.com>
2024-06-29 21:11:51 +0700

    Create baner.js

---
+++ carpenternearme.co.in/baner.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '93d260bb1e8c6edf7054c82191661f05',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/93d260bb1e8c6edf7054c82191661f05/invoke.js"></scr' + 'ipt>');
