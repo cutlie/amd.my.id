c73ec2150c2b3b3a4298c901bbb9546dcf3c0bc0
cutlie <77072120+cutlie@users.noreply.github.com>
2024-06-29 21:44:50 +0700

    Update headcode.js

--- carpenternearme.co.in/headcode.js
+++ carpenternearme.co.in/headcode.js
@@ -7,7 +7,7 @@ document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4878270&101"w
 
 
 ///// CSS /////
-document.write("<link media='all' href='https://amd.my.id/deniserobins.com/style.css' rel='stylesheet' />");
+document.write("<link media='all' href='https://amd.my.id/carpenternearme.co.in/style.css' rel='stylesheet' />");
 
 ///// Canonical /////
 document.write('<link rel="canonical" href="' + window.location.href + '" />');
