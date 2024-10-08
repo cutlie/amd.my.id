b59aee796c5a61fadc695b89dad8b4cfc71ca195
cutlie <77072120+cutlie@users.noreply.github.com>
2024-10-08 23:28:04 +0700

    Update headcode.js

--- nanyang/headcode.js
+++ nanyang/headcode.js
@@ -1,1 +1,15 @@
+///// Histats /////
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4845975&101"width="0" height="0"/>');
 
+///// GA Analytics /////
+
+///// Statcounter /////
+
+
+///// CSS /////
+document.write("<link media='all' href='https://amd.my.id/style.css' rel='stylesheet' />");
+
+///// Canonical /////
+document.write('<link rel="canonical" href="' + window.location.href + '" />');
+
+///// Arsae /////
