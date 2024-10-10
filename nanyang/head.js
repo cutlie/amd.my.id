75fd57a94acaca151ab8bd96d94dbd0a13479458
cutlie <77072120+cutlie@users.noreply.github.com>
2024-10-10 23:48:36 +0700

    Update head.js

--- nanyang/head.js
+++ nanyang/head.js
@@ -1,2 +1,2 @@
 var version = Math.floor(Math.random() * 99999);
-document.write('<script type="text/javascript" src="https://amd.my.id/nyc/headcode.js?v='+version+'"><\/script>');
+document.write('<script type="text/javascript" src="https://amd.my.id/nanyang/headcode.js?v='+version+'"><\/script>');
