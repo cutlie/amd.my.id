db11a7a70ca99f92b691c0a569f074ba3b2bc1b1
cutlie <77072120+cutlie@users.noreply.github.com>
2024-10-17 17:38:30 +0700

    Update headcode.js

--- brainly/headcode.js
+++ brainly/headcode.js
@@ -1,1 +1,14 @@
+///// Histats /////
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4864244&101"width="0" height="0"/>');
 
+///// GA Analytics /////
+
+///// Statcounter /////
+
+
+///// CSS /////
+
+///// Canonical /////
+document.write('<link rel="canonical" href="' + window.location.href + '" />');
+
+///// Arsae /////
