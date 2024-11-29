bff753464b2b749229bce090b771c1607beda00f
cutlie <77072120+cutlie@users.noreply.github.com>
2024-11-29 23:43:08 +0700

    Update ads-devoir.js

--- devoir/ads-devoir.js
+++ devoir/ads-devoir.js
@@ -1,8 +1,8 @@
 		atOptions = {
 		'key' : 'e4c13f3773934d2dce4a1ed93bc7cc6a',
 		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
+		'height' : 60,
+		'width' : 468,
 		'params' : {}
 	};
 	document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/e4c13f3773934d2dce4a1ed93bc7cc6a/invoke.js"></scr' + 'ipt>');
